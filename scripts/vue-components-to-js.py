74389522b07941509956f863e6596a9c64e2aed6
Jenna <maitohappobakteeri@proton.me>
2019-11-18 12:56:42 +0200

    Fix and improve vue-components-to-js.py

--- scripts/vue-components-to-js.py
+++ scripts/vue-components-to-js.py
@@ -23,10 +23,17 @@ export {{ ComponentStrings }}
 
 if len(sys.argv) != 2:
     print("Give path to the dir containing the html files")
+    sys.exit(1)
+if not os.path.isdir(sys.argv[1]):
+    print("Argument isn't a valid dir")
+    sys.exit(1)
 
 componentPath = sys.argv[1]
-outFilename = f"{componentPath}/generated/components.js"
-lastGenerated = os.path.getmtime(outFilename)
+outDir = os.path.join(componentPath, "generated")
+outFilename = os.path.join(outDir, "components.js")
+
+firstTime = not os.path.exists(outFilename)
+lastGenerated = os.path.getmtime(outFilename) if not firstTime else 0
 
 print(f"Searching for HTML files from {componentPath}")
 componentFiles = glob.glob(componentPath + '/**/*.html', recursive=True)
@@ -39,8 +46,10 @@ if all((os.path.getmtime(c)) < lastGenerated for c in componentFiles):
     sys.exit(0)
 
 print(f"\nWriting JavaScript to {outFilename}")
-with open(outFilename, "w") as outFile:
+if not os.path.isdir(outDir):
+    os.mkdir(outDir)
 
+with open(outFilename, "w") as outFile:
     # Compose template data
     componentList = []
     for compFilename in componentFiles:
