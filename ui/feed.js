e4497f4394065df5d728ca39156fc9b795965d14
Jenna <maitohappobakteeri@proton.me>
2019-11-18 10:03:19 +0200

    Changing selected feed

--- ui/feed.js
+++ ui/feed.js
@@ -1,4 +1,41 @@
 
+var convert = require('xml-js');
+
+function wrapXML(parsedXML) {
+  parsedXML.firstWithName = function(name) {
+    return wrapXML(parsedXML.elements.filter(e => e.name == name)[0])
+  }
+
+  parsedXML.firstWithType = function(type) {
+    return wrapXML(parsedXML.elements.filter(e => e.type == type)[0])
+  }
+
+  parsedXML.elementsWithName = function(name) {
+    return parsedXML.elements
+      .filter(e => e.name == name)
+      .map(e => wrapXML(e))
+  }
+
+  return parsedXML
+}
+
+function parseXML(xml) {
+  let data = JSON.parse(convert.xml2json(xml))
+  return wrapXML(data)
+}
+
+function processRSSResponse(xmlString) {
+  let items = parseXML(xmlString)
+    .firstWithName("rss")
+    .firstWithName("channel")
+    .elementsWithName("item")
+    .map(e => e
+      .firstWithName("title")
+      .firstWithType("text")
+      .text)
+  return items;
+}
+
 class FeedItem {
   constructor(headline) {
     this.headline = headline
@@ -10,6 +47,22 @@ class Feed {
       this.title = title
       this.url = url
       this.items = []
+    }
+
+    activate() {
+      if (this.items.length !== 0) {
+        console.log("Items already fetched")
+        return;
+      }
+
+      fetch(this.url)
+        .then(response => response.text()
+          .then(txt => processRSSResponse(txt)
+            .forEach(title => this.items.push(new FeedItem(title)))))
+    }
+
+    setAsCurrent() {
+      console.error("Unbinded")
     }
 }
 
--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,6 @@ import  { Feed, FeedItem } from "./feed.js";
 import { ComponentStrings } from "./components/generated/components.js";
 
 var Vue = require('vue/dist/vue.js')
-var convert = require('xml-js');
 
 ComponentStrings.forEach(c => {
   Vue.component(c.name, {
@@ -18,8 +17,15 @@ var feeds = JSON.parse(fs.readFileSync('test/testfeeds.json', 'utf8'))
 
 let viewmodel = {
   feeds: feeds,
-  current: feeds[0]
+  current: undefined
 }
+
+feeds.forEach(f => {
+  f.setAsCurrent = function() {
+    viewmodel.current = f
+    f.activate()
+  }
+})
 
 var vapp = new Vue({
   el: '#main',
@@ -29,41 +35,4 @@ var vapp = new Vue({
   }
 })
 
-function wrapXML(parsedXML) {
-  parsedXML.firstWithName = function(name) {
-    return wrapXML(parsedXML.elements.filter(e => e.name == name)[0])
-  }
-
-  parsedXML.firstWithType = function(type) {
-    return wrapXML(parsedXML.elements.filter(e => e.type == type)[0])
-  }
-
-  parsedXML.elementsWithName = function(name) {
-    return parsedXML.elements
-      .filter(e => e.name == name)
-      .map(e => wrapXML(e))
-  }
-
-  return parsedXML
-}
-
-function parseXML(xml) {
-  let data = JSON.parse(convert.xml2json(xml))
-  return wrapXML(data)
-}
-
-function processRSSResponse(xmlString) {
-  let items = parseXML(xmlString)
-    .firstWithName("rss")
-    .firstWithName("channel")
-    .elementsWithName("item")
-    .map(e => e
-      .firstWithName("title")
-      .firstWithType("text")
-      .text)
-  items.forEach(i => viewmodel.current.items.push(new FeedItem(i)))
-}
-
-fetch(viewmodel.current.url)
-  .then(response => response.text()
-    .then(txt => processRSSResponse(txt)))
+feeds[0].setAsCurrent()
