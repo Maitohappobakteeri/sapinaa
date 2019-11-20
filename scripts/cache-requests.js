1eaf67181241ddb941f790b5dfbbc77336f41bca
Jenna <maitohappobakteeri@proton.me>
2019-11-20 03:55:10 +0200

    More logging in cache-request.js

--- scripts/cache-requests.js
+++ scripts/cache-requests.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var fetch = require("node-fetch");
+var colors = require('colors');
 
 function readJSON(filename) {
   return JSON.parse(fs.readFileSync(filename, 'utf8'));
@@ -12,12 +13,18 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function cacheFeeds(feeds) {
+async function cacheFeeds(urls) {
   console.log("Creating feed response cache");
   let responses = {};
+
+  console.log("Found following URLs");
   console.log(urls);
   for (let i in urls) {
     let url = urls[i];
+    console.log(
+      "Fetching " + (parseInt(i) + 1) + "/" + urls.length
+      + " " + url.blue
+    );
     let response = await fetch(url).then(response => response.text());
     responses[url] = response;
     await sleep(2000);
