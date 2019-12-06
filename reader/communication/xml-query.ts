f704590180537f1d65d6f9787a3d8bc74cade1c3
Jenna <maitohappobakteeri@proton.me>
2019-12-06 16:06:50 +0200

    Fix some ts warnings

--- reader/communication/xml-query.ts
+++ reader/communication/xml-query.ts
@@ -1,38 +1,40 @@
 var convert = require("xml-js");
 
 // Parse XML to JSON and wrap it in query functions
-function parseXML(xml) {
+function parseXML(xml: string) {
   let data = JSON.parse(convert.xml2json(xml));
   return wrapXML(data);
 }
 
-function wrapXML(xmlObj) {
+function wrapXML(xmlObj: any) {
   if (xmlObj === undefined) {
     xmlObj = {};
   }
 
-  xmlObj.firstWithName = function(name) {
+  xmlObj.firstWithName = function(name: string) {
     if (xmlObj.elements === undefined) {
       return wrapXML(undefined);
     }
 
-    return wrapXML(xmlObj.elements.filter(e => e.name == name)[0]);
+    return wrapXML(xmlObj.elements.filter((e: any) => e.name == name)[0]);
   };
 
-  xmlObj.firstWithType = function(type) {
+  xmlObj.firstWithType = function(type: string) {
     if (xmlObj.elements === undefined) {
       return wrapXML(undefined);
     }
 
-    return wrapXML(xmlObj.elements.filter(e => e.type == type)[0]);
+    return wrapXML(xmlObj.elements.filter((e: any) => e.type == type)[0]);
   };
 
-  xmlObj.elementsWithName = function(name) {
+  xmlObj.elementsWithName = function(name: string) {
     if (xmlObj.elements === undefined) {
       return wrapXML([]);
     }
 
-    return xmlObj.elements.filter(e => e.name == name).map(e => wrapXML(e));
+    return xmlObj.elements
+      .filter((e: any) => e.name == name)
+      .map((e: any) => wrapXML(e));
   };
 
   return xmlObj;
